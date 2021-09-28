8b6c87eff844c9f2fc3092d6f5697034723df1eb
Elisa <elisacanyelles@gmail.com>
2021-09-28 22:15:32 +0200

    added dependency

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const studentInfo = require('./userInformation');
 console.log(`Hi, my name is ${studentInfo.name} and I study at ${studentInfo.campus}`);
 
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
